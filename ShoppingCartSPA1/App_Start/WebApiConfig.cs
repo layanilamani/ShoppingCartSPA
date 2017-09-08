9a09ce4b08b5df0cb0ba196ad5529903c799b7a7
layanilamani <30865389+layanilamani@users.noreply.github.com>
2017-09-07 23:05:08 -0700

    Update WebApiConfig.cs

--- ShoppingCartSPA1/App_Start/WebApiConfig.cs
+++ ShoppingCartSPA1/App_Start/WebApiConfig.cs
@@ -9,6 +9,9 @@ namespace ShoppingCartSPA1
 {
     public static class WebApiConfig
     {
+        public static string UrlPrefix { get { return "api"; } }
+        public static string UrlPrefixRelative { get { return "~/api"; } }
+
         public static void Register(HttpConfiguration config)
         {  
             
