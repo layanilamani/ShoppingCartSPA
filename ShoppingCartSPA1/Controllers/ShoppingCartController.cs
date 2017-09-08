bd44b400dd41a97544ab5b2444a11f119a710c2a
layanilamani <30865389+layanilamani@users.noreply.github.com>
2017-09-07 23:05:47 -0700

    Update ShoppingCartController.cs

--- ShoppingCartSPA1/Controllers/ShoppingCartController.cs
+++ ShoppingCartSPA1/Controllers/ShoppingCartController.cs
@@ -7,16 +7,43 @@ using System.Web.Http;
 
 namespace ShoppingCartSPA1.Controllers
 {
+    public class Item
+    {
+        public int Count { get; set; }
+    }
+    public class ShoppingCart
+    {
+        public List<Item> Items { get; set; }
+
+    }
     public class ShoppingCartController : ApiController
     {
+
         [Route("api/GetProducts")]
         [HttpGet]
         public List<Product> GetProducts()
         {
-            ShoppingCartEntities db = new ShoppingCartEntities();
+            var db = new ShoppingCartEntities();
             db.Configuration.ProxyCreationEnabled = false;
 
             return db.Products.ToList();
+        }
+
+        public void AddToCart(Item item)
+        {
+            ShoppingCart sc = null;
+
+            if (HttpContext.Current.Session["sc"] == null)
+                sc = new ShoppingCart();
+            else
+                sc = HttpContext.Current.Session["sc"] as ShoppingCart;
+
+            if (sc.Items == null)
+                sc.Items = new List<Item>();
+
+            sc.Items.Add(item);
+
+            HttpContext.Current.Session["sc"] = sc;
         }
     }
 }
