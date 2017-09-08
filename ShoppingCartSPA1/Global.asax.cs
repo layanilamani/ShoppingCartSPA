0b82ddbaa2e080349b015e44f020820b1f2452cd
layanilamani <30865389+layanilamani@users.noreply.github.com>
2017-09-07 23:04:19 -0700

    Update Global.asax.cs

--- ShoppingCartSPA1/Global.asax.cs
+++ ShoppingCartSPA1/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace ShoppingCartSPA1
 {
@@ -20,4 +21,17 @@ namespace ShoppingCartSPA1
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
+    
+      protected void Application_PostAuthorizeRequest()
+        {
+            if (IsWebApiRequest())
+            {
+                HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+            }
+        }
+
+        private bool IsWebApiRequest()
+        {
+            return HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.StartsWith(WebApiConfig.UrlPrefixRelative);
+        }
 }
